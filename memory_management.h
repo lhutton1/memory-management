e868a7a2ab168867b66dbc96e9a67f0f8697cbf8
sc17ljth <sc17ljth@leeds.ac.uk>
2018-10-26 11:19:52 +0100

    initial

--- memory_management.h
+++ memory_management.h
@@ -1,7 +1,33 @@
-#include <sys/types.h>
+#ifndef MEMORY_MANAGEMENT_H
+#define MEMORY_MANAGEMENT_H
 
-void * _malloc(size_t size);
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-void _free(void * ptr);
+// Datatype that stores data about the block.
+// Size - size of the block (not including the metadata)
+// Free - has the block been freed by the process?
+// Prev - the previous block in the linked list
+// Next - the next block in the linked list
+typedef struct block_meta {
+  size_t size;
+  bool free;
+  struct block_meta *prev;
+  struct block_meta *next;
+} block_meta;
 
+// size of the metadata used in each block.
+const int META_SIZE = 16;
 
+// head of doubly linked list block.
+block_meta *block_head = NULL;
+
+// Allocates new block of memory.
+void *_malloc(size_t size);
+
+// Frees a block of memory.
+void _free(void *ptr);
+
+#endif
